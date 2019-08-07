d00cea7a3ebf3be519038333e0f5cdf8e3e478bb
MagicBus <alfonsoj.barrio@gmail.com>
2019-08-07 16:04:22 +0400

    s

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 function doRegisterPWAApp(){
-	alert("let's go 2");
+	if ('serviceWorker' in navigator) {
+	  window.addEventListener('load', function() {
+	    navigator.serviceWorker.register('/sw.js').then(
+	    	function(registration) {
+		      // Registration was successful
+		      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+		    }, function(err) {
+		      // registration failed :(
+		      console.log('ServiceWorker registration failed: ', err);
+		    });
+	  });
+	}
 
 }
